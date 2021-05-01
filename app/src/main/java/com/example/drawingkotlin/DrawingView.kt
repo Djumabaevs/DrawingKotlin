c01c68e578dfa6e0d69b751afa810b805f9183a2
Bakyt <djumabaevb@gmail.com>
2021-05-01 11:32:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/drawingkotlin/DrawingView.kt
@@ -0,0 +1,15 @@
+package com.example.drawingkotlin
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private var mDrawPath: CustomPath? = null
+
+    internal inner class CustomPath {
+
+    }
+
+}
